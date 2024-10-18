2899304b20622dfb27c8488e4a49dfdb4bda790b
Renem24 <106603073+Renem24@users.noreply.github.com>
2024-10-19 00:49:25 +0900

    Quartz sync: Oct 19, 2024, 12:49 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Renem's Knowledge",
+    pageTitle: "Renem24's Knowledge",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
